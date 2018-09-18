51bbf6e5a363969ef7e58676a1d2f45e0e63a4c2
Cory P <32197027+Cpease2010@users.noreply.github.com>
2018-09-18 09:52:44 -0600

    When pease attacks (#3)
    * basics of the DOM
    
    * added text to each div

--- sept-18-warmup/app.js
+++ sept-18-warmup/app.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const div_target = document.querySelectorAll('div')
+    div_target.forEach(div => {
+        div.innerHTML = "Can't Touch This ☄️"
+    });
 })
